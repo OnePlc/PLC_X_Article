c47ff9eae8bc1ad3ca4a9bee6a1c563497255cd8
JBinggi <juerg.binggeli@gmail.com>
2020-03-06 01:57:57 +0100

    add language file - Gutschein

--- data/data_shop.sql
+++ data/data_shop.sql
@@ -5,7 +5,7 @@
 INSERT INTO `core_form_field` (`Field_ID`, `type`, `label`, `fieldkey`, `tab`, `form`, `class`, `url_view`, `url_list`, `show_widget_left`, `allow_clear`, `readonly`, `tbl_cached_name`, `tbl_class`, `tbl_permission`) VALUES
 (NULL, 'multiselect', 'Categories', 'category_idfs', 'article-base', 'article-single', 'col-md-2', '', '/tag/api/list/article-single/category', 0, 1, 0, 'entitytag-single', 'OnePlace\\Tag\\Model\\EntityTagTable','add-OnePlace\\Article\\Controller\\CategoryController'),
 (NULL, 'textarea', 'Description', 'description', 'article-base', 'article-single', 'col-md-12', '', '', 0, 1, 0, '', '', ''),
-(NULL, 'featuredimage', 'Featured Image', 'featured_image', 'article-base', 'event-single', 'col-md-3', '', '', '0', '1', '0', '', '', ''),
+(NULL, 'featuredimage', 'Featured Image', 'featured_image', 'article-base', 'article-single', 'col-md-3', '', '', '0', '1', '0', '', '', ''),
 (NULL, 'gallery', 'Gallery', 'gallery', 'article-gallery', 'article-single', 'col-md-12', '', '', '0', '1', '0', '', '', ''),
 (NULL, 'partial', 'Web Gallery', 'webgallery', 'article-gallerysort', 'article-single', 'col-md-12', '', '', '0', '1', '0', '', '', '');
 --
@@ -20,3 +20,8 @@ INSERT INTO `permission` (`permission_key`, `module`, `label`, `nav_label`, `nav
 INSERT INTO core_form_tab (Tab_ID, form, title, subtitle, icon, counter, sort_id, filter_check, filter_value) VALUES
 ('article-gallery', 'article-single', 'Gallery', 'Images', 'fas fa-images', '', '4', '', ''),
 ('article-gallerysort', 'article-single', 'Gallery Sorting', 'sorted', 'fas fa-images', '', '4', '', '');
+
+--
+-- module icon
+--
+INSERT INTO `settings` (`settings_key`, `settings_value`) VALUES ('article-icon', 'fas fa-tag');
--- data/structure_shop.sql
+++ data/structure_shop.sql
@@ -1,1 +1,2 @@
-ALTER TABLE `article` ADD `description` TEXT NOT NULL AFTER `label`;
+ALTER TABLE `article` ADD `description` TEXT NOT NULL AFTER `label`,
+ADD `featured_image` VARCHAR(255) NOT NULL DEFAULT '' AFTER `description`;
