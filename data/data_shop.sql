f0e0858b6ca7083285b4af911004f3b0c0bac2e2
Praesidiarius <n.kammermann@swiss-crypto-mining.com>
2020-03-07 18:28:17 +0100

    web show fix

--- data/data_shop.sql
+++ data/data_shop.sql
@@ -8,7 +8,7 @@ INSERT INTO `core_form_field` (`Field_ID`, `type`, `label`, `fieldkey`, `tab`, `
 (NULL, 'featuredimage', 'Featured Image', 'featured_image', 'article-base', 'article-single', 'col-md-3', '', '', '0', '1', '0', '', '', ''),
 (NULL, 'gallery', 'Gallery', 'gallery', 'article-gallery', 'article-single', 'col-md-12', '', '', '0', '1', '0', '', '', ''),
 (NULL, 'partial', 'Web Gallery', 'webgallery', 'article-gallerysort', 'article-single', 'col-md-12', '', '', '0', '1', '0', '', '', ''),
-(NULL, 'select', 'web show', 'web_show_idfs', 'article-base', 'article-single', 'col-md-2', '', '/application/selectbool', '', '0', '1', '0', '', 'OnePlace\\BoolSelect', ''),
+(NULL, 'select', 'web show', 'show_on_web_idfs', 'article-base', 'article-single', 'col-md-2', '', '/application/selectbool', '', '0', '1', '0', '', 'OnePlace\\BoolSelect', ''),
 (NULL, 'select', 'web spotlight', 'web_spotlight_idfs', 'article-base', 'article-single', 'col-md-2', '', '/application/selectbool', '', '0', '1', '0', '', 'OnePlace\\BoolSelect', '');
 --
 -- Permissions
--- data/structure_shop.sql
+++ data/structure_shop.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `article` ADD `description` TEXT NOT NULL AFTER `label`,
 ADD `featured_image` VARCHAR(255) NOT NULL DEFAULT '' AFTER `description`,
-ADD `web_show_idfs` TINYINT(1) NOT NULL DEFAULT '0' AFTER `label`,
+ADD `show_on_web_idfs` TINYINT(1) NOT NULL DEFAULT '0' AFTER `label`,
 ADD `web_spotlight_idfs` TINYINT(1) NOT NULL DEFAULT '0' AFTER `web_show_idfs`;
