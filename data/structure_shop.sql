f701af10fe8ed6ac9ee76d5e7e09f1081f2badfc
Praesidiarius <n.kammermann@swiss-crypto-mining.com>
2020-03-22 05:00:53 +0100

    shop fixes

--- data/structure_shop.sql
+++ data/structure_shop.sql
@@ -2,5 +2,6 @@ ALTER TABLE `article` ADD `description` TEXT NOT NULL AFTER `label`,
 ADD `featured_image` VARCHAR(255) NOT NULL DEFAULT '' AFTER `description`,
 ADD `show_on_web_idfs` TINYINT(1) NOT NULL DEFAULT '0' AFTER `label`,
 ADD `web_spotlight_idfs` TINYINT(1) NOT NULL DEFAULT '0' AFTER `show_on_web_idfs`,
+ADD `web_sort_id` INT(11) NOT NULL DEFAULT '0' AFTER `show_on_web_idfs`,
 ADD `ref_idfs` INT(11) NOT NULL DEFAULT '0' AFTER `web_spotlight_idfs`,
 ADD `ref_type` VARCHAR(50) NOT NULL DEFAULT '' AFTER `ref_idfs`;
